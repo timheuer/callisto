a4bdec0fd501a6d2ba524186be5f1ad742177e7e
Tim Heuer <tim@timheuer.com>
2012-03-23 23:22:24 -0700

    Adding stub logos to remove Windows logo from VSIX

--- buildvsix.bat
+++ buildvsix.bat
@@ -7,6 +7,8 @@ mkdir src\Callisto.VSIX\Redist\CommonConfiguration\neutral\Themes\
 mkdir src\Callisto.VSIX\References\CommonConfiguration\neutral\
 copy License.txt src\Callisto.VSIX\License.txt
 copy SDKManifest.xml src\Callisto.VSIX\SDKManifest.xml
+copy CallistoMoon.png src\Callisto.VSIX\CallistoMoon.png
+copy CallistoLogo.png src\Callisto.VSIX\CallistoLogo.png
 copy src\Callisto\bin\release\Callisto.dll src\Callisto.VSIX\References\CommonConfiguration\neutral\
 copy src\Callisto\bin\release\Callisto.pri src\Callisto.VSIX\Redist\CommonConfiguration\neutral\
 copy src\Callisto\bin\release\themes\generic.xaml src\Callisto.VSIX\Redist\CommonConfiguration\neutral\Themes\
