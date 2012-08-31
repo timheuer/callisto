aff3b30f7e9144fe6334d81bd4004f8cf6412039
Tim Heuer <tim@timheuer.com>
2012-08-30 23:04:47 -0700

    Created FlipViewIndicator control

--- src/Callisto.TestApp/MainPage.xaml.cs
+++ src/Callisto.TestApp/MainPage.xaml.cs
@@ -51,6 +51,7 @@ namespace Callisto.TestApp
 				Samples.Add(new SamplePage() { Name = "LiveTile", Page = typeof(SamplePages.LiveTileSample) });
                 Samples.Add(new SamplePage() { Name = "Rating", Page = typeof(SamplePages.RatingControl) });
                 Samples.Add(new SamplePage() { Name = "WebViewExtension", Page = typeof(SamplePages.WebViewExtension) });
+                Samples.Add(new SamplePage() { Name = "FlipViewIndicator", Page = typeof(SamplePages.FlipViewIndicatorSample) });
 				this.DataContext = Samples;
 			}
 		}
---
+++ src/Callisto/Controls/FlipViewIndicator/FlipViewIndicator.cs
@@ -0,0 +1,61 @@
+﻿﻿//
+// Copyright (c) 2012 Tim Heuer
+//
+// Licensed under the Microsoft Public License (Ms-PL) (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://opensource.org/licenses/Ms-PL.html
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+namespace Callisto.Controls
+{
+    public sealed class FlipViewIndicator : ListBox
+    {
+        public FlipViewIndicator()
+        {
+            this.DefaultStyleKey = typeof(FlipViewIndicator);
+        }
+
+        public FlipView FlipView
+        {
+            get { return (FlipView)GetValue(FlipViewProperty); }
+            set { SetValue(FlipViewProperty, value); }
+        }
+
+        public static readonly DependencyProperty FlipViewProperty =
+            DependencyProperty.Register("FlipView", typeof(FlipView), typeof(FlipViewIndicator), new PropertyMetadata(null, (depobj, args) =>
+                {
+                    FlipViewIndicator fvi = (FlipViewIndicator)depobj;
+                    FlipView fv = (FlipView)args.NewValue;
+
+                    // set the binding context to the same as the associated FlipView
+                    fvi.ItemsSource = fv.ItemsSource;
+
+                    // create the element binding source
+                    Binding eb = new Binding();
+                    eb.Mode = BindingMode.TwoWay;
+                    eb.Source = fv;
+                    eb.Path = new PropertyPath("SelectedItem");
+
+                    // set the element binding to change selection when the FlipView changes
+                    fvi.SetBinding(FlipViewIndicator.SelectedItemProperty, eb);
+                }));
+    }
+}
